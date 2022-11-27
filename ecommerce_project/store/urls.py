07946bf4ec04ac365b68251cf7e632fb3f1eaa5e
zia <zia166@gmail.com>
2022-11-27 12:14:20 +0000

    second-commit

--- ecommerce_project/store/urls.py
+++ ecommerce_project/store/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.urls import path
 from . import views
 from .views import CustomLoginView
@@ -17,23 +16,3 @@ urlpatterns = [
     path("checkout/", views.checkoutPage, name="checkout"),
     path("process-order/", views.processOrder, name="process-order"),
 ]
-=======
-from django.urls import path
-from . import views
-from .views import CustomLoginView
-from django.contrib.auth.views import LogoutView
-
-urlpatterns = [
-    path("login/", CustomLoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(next_page="home"), name="logout"),
-    path("", views.HomePage, name="home"),
-    path("store/<int:pk>", views.ProductList.as_view(), name="store"),
-    path("store/", views.store, name="store1"),
-    path("cart/<int:pk>/", views.OrderList.as_view(), name="cart"),
-    path("cart/", views.cart, name="cart1"),
-    # path("Create/<int:pk>/", views.OrderCreateView.as_view(), name="create"),
-    path("update_item/", views.updateItem, name="update_item"),
-    path("checkout/", views.checkoutPage, name="checkout"),
-    path("process-order/", views.processOrder, name="process-order"),
-]
->>>>>>> f88bcbe091498837b2a27a2ab727d23196ca8348
