2e6fef7acabb2014659ad4a75cc10724bff94607
zia166 <105048583+zia166@users.noreply.github.com>
2022-11-27 10:21:13 +0000

    Add files via upload

--- ecommerce_project/store/urls.py
+++ ecommerce_project/store/urls.py
@@ -8,7 +8,11 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(next_page="home"), name="logout"),
     path("", views.HomePage, name="home"),
     path("store/<int:pk>", views.ProductList.as_view(), name="store"),
+    path("store/", views.store, name="store1"),
     path("cart/<int:pk>/", views.OrderList.as_view(), name="cart"),
-    path("Create/<int:pk>/", views.OrderCreateView.as_view(), name="create"),
-    # path("cart/", views.cart, name="cart"),
+    path("cart/", views.cart, name="cart1"),
+    # path("Create/<int:pk>/", views.OrderCreateView.as_view(), name="create"),
+    path("update_item/", views.updateItem, name="update_item"),
+    path("checkout/", views.checkoutPage, name="checkout"),
+    path("process-order/", views.processOrder, name="process-order"),
 ]
